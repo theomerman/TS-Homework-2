211dd77be58cf909333d4ff3d7c9c4cfc8ebf37d
theomerman <edsonanime@hotmail.es>
2024-11-06 19:46:43 -0600

    tailwind imported, createTask function added

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,42 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+export function addTask(divTasks: HTMLHtmlElement) {
+  let lastTask = divTasks.children.item(divTasks.children.length - 1);
+  let newTask = document.createElement("div");
+  let id = lastTask?.getAttribute("id");
+  let newId = 0;
+  if (id) {
+    newId = Number(id) + 1;
+    newTask.setAttribute("id", newId.toString());
+  } else {
+    newTask.setAttribute("id", "0");
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+  newTask.setAttribute(
+    "class",
+    "flex w-auto min-h-20 bg-slate-300 rounded-lg px-6 mx-6 gap-3 justify-between items-center hover:shadow-lg",
+  );
+  let taskText = document.createTextNode(
+    (document.getElementById("inputText") as HTMLInputElement).value,
+  );
+  let p = document.createElement("button");
+  p.appendChild(taskText);
+  let button = document.createElement("button");
+  button.appendChild(document.createTextNode("Delete"));
+  button.setAttribute(
+    "class",
+    "w-auto h-fit p-2 bg-red-500 rounded-lg hover:drop-shadow-lg",
+  );
+  button.addEventListener("click", () => removeChild(newId));
+  newTask.appendChild(p);
+  newTask.appendChild(button);
+  divTasks.appendChild(newTask);
+  (document.getElementById("inputText") as HTMLInputElement).value = "";
+}
+function removeChild(id: number) {
+  let element = document.getElementById(id.toString());
+  element?.remove();
+}
+export function addButton(
+  button: HTMLButtonElement,
+  divTasks: HTMLHtmlElement,
+) {
+  button.addEventListener("click", () => addTask(divTasks));
 }
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { addButton } from "./counter";
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+<section class="flex flex-col gap-5 text-center items-center  mx-auto mt-52 py-6 bg-gray-100 rounded-lg text-white">
+    <h1 class="bg-gray-100 w-full text-sky-700 font-bold text-5xl">Todo App!</h1>
+    <div id="tasks" class="flex flex-col gap-3 w-full h-full bg-gray-100 overflow-auto text-2xl">
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+    <input id="inputText" type="text" class="w-11/12 h-20 bg-gray-200 text-black text-center text-2xl rounded-lg text-zinc-500" 
+        placeholder="Enter your task here" />
+    <button id="add-task-button" class="w-11/12 h-20 bg-blue-500 text-2xl rounded-lg">Add Task</button>
+</section>
+`;
+addButton(
+  document.querySelector("#add-task-button")!,
+  document.querySelector<HTMLHtmlElement>("#tasks")!,
+);
